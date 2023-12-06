ab9a0c8ba2d83e2cc4367be0b03773d6958ab3f7
Ross Hannon <G00381859@gmit.ie>
2023-12-06 14:41:57 +0000

    footer.css and js files created

---
+++ src/Components/footer.js
@@ -0,0 +1,14 @@
+// Footer.js
+
+import React from 'react';
+import './footer.css'; // Import the CSS file for styling
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2023 Your Website. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
